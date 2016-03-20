520b1462f3b21f6c85e7e530ad262fb8356b7871
Andreas Andersson <aa@appinmed.com>
2016-03-20 16:45:05 +0100

    Initial Commit

---
+++ S02L30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S02L30
+//
+//  Created by Andreas Andersson on 20/03/16.
+//  Copyright © 2016 Andreas Andersson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
